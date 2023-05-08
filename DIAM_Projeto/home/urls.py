64a29c15620d099c2d401fd9f11c57c77f03b381
TiagoZheng <tiago.zheng23@gmail.com>
2023-05-08 19:17:48 +0100

    new_post, login, register, logout, profile

--- DIAM_Projeto/home/urls.py
+++ DIAM_Projeto/home/urls.py
@@ -1,14 +1,30 @@
 from django.urls import include, path
 from . import views
 
-
 app_name = 'home'
+
 urlpatterns = [
     # ex: home/
     path('', views.index, name='index'),
 
-    # ex: home/login/
-    path('login/', views.login, name='login'),
+    # ex: home/1
+    path('<int:post_id>/', views.details, name='details'),
 
-    path('registration/', views.registration, name='registration'),
+    # ex: home/1/results
+    path('<int:post_id>/results', views.results, name='results'),
+
+    # ex: home/login_view/
+    path('login/', views.login_view, name='login'),
+
+    # ex: home/registration
+    path('register/', views.register, name='register'),
+
+    # ex: home/new_post/
+    path('new_post/', views.new_post, name='new_post'),
+
+    # ex: home/logout
+    path('logout/', views.user_logout, name='logout'),
+
+    # ex: home/profile
+    path('profile/', views.profile, name='profile'),
 ]
--- DIAM_Projeto/home/views.py
+++ DIAM_Projeto/home/views.py
@@ -1,15 +1,37 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    latest_post_list = Post.objects.order_by('-post_time')[:5]
+    context = {'latest_post_list': latest_post_list, }
+    return render(request, 'home/index.html', context)
 
 
-def login(request):
+def details(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    # return HttpResponse("Esta e a questao %s." % questao_id)
+    return render(request, 'home/index', {'post': post})
+
+
+def results(request, post_id):
+    response = "Estes sao os resultados da questao %s."
+    return HttpResponse(response % post_id)
+
+
+def likes(request, post_id):
+    return HttpResponse("Votacao na questao %s." % post_id)
+
+
+def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +39,7 @@ def login(request):
         if user is not None:
             login(request, user)
             # Redirect to success page or some other URL TODO
-            return redirect('success')
+            return HttpResponseRedirect(reverse('home:index'))
         else:
             # Return invalid an invalid login message
             return render(request, 'home/login.html', {'error': 'Invalid login credentials.'})
@@ -25,7 +47,7 @@ def login(request):
         return render(request, 'home/login.html')
 
 
-def registration(request):
+def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -34,16 +56,39 @@ def registration(request):
 
         # Check if password matches
         if confirm_password != password:
-            return render(request, 'home/registration.html', {'error': 'Password does not match.'})
+            return render(request, 'home/register.html', {'error': 'Password does not match.'})
 
         # Create the user
         try:
             user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
         except:
-            return render(request, 'home/registration.html', {'error': 'Failed to register'})
+            return render(request, 'home/register.html', {'error': 'Failed to register'})
 
         # If success
-        return redirect('success')
+        return HttpResponseRedirect(reverse('home:index'))
 
     else:
-        return render(request, 'home/registration.html')
+        return render(request, 'home/register.html')
+
+
+def new_post(request):
+    if request.method == 'POST':
+        post_content = request.POST['new_post']
+        post_time = timezone.now()
+        post_new = Post(post_content=post_content, post_time=post_time)
+        post_new.save()
+        return HttpResponseRedirect(reverse('home:index'))
+    else:
+        return render(request, 'home/new_post.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home:index')
+
+
+@login_required
+def profile(request):
+    user = request.user
+    return render(request, 'home/profile.html', {'user': user})
